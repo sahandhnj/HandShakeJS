77dfb469e2a7f0f6a8e0549191a31bd8ce8ea72f
Sahand <sahand@ilumy.com>
2016-05-23 11:13:57 +0200

    gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -52,26 +52,12 @@ gulp.task('build', [
 ]);
 
 gulp.task('bump', function() {
-
-  var questions = [
-    {
-      type: 'input',
-      name: 'bump',
-      message: 'Are you sure you want to bump the patch version? [Y/N]'
-    }
-  ];
-
-  inquirer.prompt( questions, function( answers ) {
-    if(answers.bump === 'Y') {
-
-      return gulp.src(['./package.json', './bower.json'])
-          .pipe(bump({type: 'patch'}))
-          .pipe(gulp.dest('./'))
-          .pipe(git.commit('bump patch version'))
-          .pipe(filter('package.json'))  // read package.json for the new version
-          .pipe(tagVersion());           // create tag
-    }
-  });
+    return gulp.src(['./package.json', './bower.json'])
+        .pipe(bump({type: 'patch'}))
+        .pipe(gulp.dest('./'))
+        .pipe(git.commit('bump patch version'))
+        .pipe(filter('package.json'))  // read package.json for the new version
+        .pipe(tagVersion());           // create tag
 });
 
 // watch scripts, styles, and templates
